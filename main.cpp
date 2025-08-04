4def497f2b092550b81619513b720c675250413f
MaxLL04 <maxwell.olson@jacks.sdstate.edu>
2025-08-04 10:18:27 -0500

    Just the framework

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+std:: iostream;
+
+
+int main(){
+    
+    cout << "Add new event or read date" << endl;
+
+
+
+
+}
